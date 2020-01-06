fe2297047849d5769150eb4e66d61a7d6e8f8bf6
Aleksey Shapoval <alex@negativeimprint.com>
2020-01-06 16:15:01 -0500

    saved video to video library

--- RWVideoNinja/RecordVideoViewController.swift
+++ RWVideoNinja/RecordVideoViewController.swift
@@ -4,11 +4,52 @@
 //  Copyright © 2020 VoVa LLC. All rights reserved.
 
 import UIKit
+import AVKit
+import MobileCoreServices
 
 class RecordVideoViewController: UIViewController {
   
   @IBAction func record(_ sender: AnyObject) {
+    VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
+  }
+  
+  @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+    let title = (error == nil) ? "Success" : "Error"
+    let message = (error == nil) ? "Video was saved" : "Video failed to save"
+    
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil))
+    present(alert, animated: true, completion: nil)
+  }
+  
+  
+}
+
+extension RecordVideoViewController: UIImagePickerControllerDelegate {
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    dismiss(animated: true, completion: nil)
+    
+    guard
+      let mediaType = info[UIImagePickerControllerMediaType] as? String,
+      mediaType == (kUTTypeMovie as? String),
+      let url = info[UIImagePickerControllerMediaURL] as? URL,
+      UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+      else {
+        print("Failed to identify image mediaType & url")
+        return
+    }
+    
+    //Handle a movie capture
+    UISaveVideoAtPathToSavedPhotosAlbum(url.path,
+                                        self,
+                                        #selector(video(_:didFinishSavingWithError:contextInfo:)),
+                                        nil)
     
   }
+  
+}
+
+extension RecordVideoViewController: UINavigationControllerDelegate {
   
 }
