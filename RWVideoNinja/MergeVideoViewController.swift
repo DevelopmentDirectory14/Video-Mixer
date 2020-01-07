faba1b2f1b3d04da5bcb8a84d4170d747cd4817b
Aleksey Shapoval <alex@negativeimprint.com>
2020-01-07 13:35:34 -0500

    solved "This application is modifying the autolayout engine from a background thread" issue by wrapping in asyn code block for the alert presentation.  However, now the video merge is failing and I am getting a fail alert.

--- RWVideoNinja/MergeVideoViewController.swift
+++ RWVideoNinja/MergeVideoViewController.swift
@@ -25,6 +25,47 @@ class MergeVideoViewController: UIViewController {
     return false
   }
   
+    func exportDidFinish(_ session: AVAssetExportSession) {
+      //Cleanup assets
+      activityMonitor.stopAnimating()
+      firstAsset = nil
+      secondAsset = nil
+      audioAsset = nil
+      
+      guard
+          session.status == AVAssetExportSessionStatus.completed,
+          let outputURL = session.outputURL
+          else { return }
+      
+      let saveVideoToPhotos =  {
+          PHPhotoLibrary.shared().performChanges({
+              PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: outputURL)
+          }) { saved, error in
+              let success = saved && (error == nil)
+              let title = success ? "Success" : "Error"
+              let message = success ? "Video saved" : "Failed to save video"
+              
+            DispatchQueue.main.async {
+              let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+              alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil))
+              self.present(alert, animated: true, completion: nil)
+            }
+          }
+      }
+      
+      //Ensure permission to access photo library
+      if PHPhotoLibrary.authorizationStatus() != .authorized {
+          PHPhotoLibrary.requestAuthorization({ status in
+              if status == .authorized {
+                  saveVideoToPhotos()
+              }
+          })
+      } else {
+          saveVideoToPhotos()
+      }
+    }
+    
+    
   @IBAction func loadAssetOne(_ sender: AnyObject) {
     if savedPhotosAvailable() {
       loadingAssetOne = true
@@ -47,49 +88,85 @@ class MergeVideoViewController: UIViewController {
   }
     
   @IBAction func merge(_ sender: AnyObject) {
-    
-  }
-  
-  func exportDidFinish(_ session: AVAssetExportSession) {
-    //Cleanup assets
-    activityMonitor.stopAnimating()
-    firstAsset = nil
-    secondAsset = nil
-    audioAsset = nil
-    
     guard
-        session.status == AVAssetExportSessionStatus.completed,
-        let outputURL = session.outputURL
+        let firstAsset = firstAsset,
+        let secondAsset = secondAsset
         else { return }
     
-    let saveVideoToPhotos =  {
-        PHPhotoLibrary.shared().performChanges({
-            PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: outputURL)
-        }) { saved, error in
-            let success = saved && (error == nil)
-            let title = success ? "Success" : "Error"
-            let message = success ? "Video saved" : "Failed to save video"
-            
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+    activityMonitor.startAnimating()
+    
+    //1 - Create AVMutableComposition object.  This object will hold your AVMutableCompositionTrack instances.
+    let mixComposition = AVMutableComposition()
+    
+    //2 - Create two video tracks.
+    guard
+        let firstTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID:
+    Int32(kCMPersistentTrackID_Invalid))
+        else { return }
+    
+    do {
+        try firstTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, firstAsset.duration),
+                                        of: firstAsset.tracks(withMediaType:
+                                            .video)[0],
+                                        at: kCMTimeZero)
+    } catch {
+        print("Failed to load first track")
+        return
+    }
+    
+    guard
+        let secondTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID:
+    Int32(kCMPersistentTrackID_Invalid))
+        else { return }
+    
+    do {
+        try secondTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, secondAsset.duration),
+                                        of: secondAsset.tracks(withMediaType:
+                                            .video)[0],
+                                        at: firstAsset.duration)
+    } catch {
+        print("Failed to load second track")
+        return
+    }
+    
+    // 3 - Audio track
+    if let loadedAudioAsset = audioAsset {
+        let audioTrack = mixComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: 0)
+        do {
+            try audioTrack?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, CMTimeAdd(firstAsset.duration, secondAsset.duration)),
+                                            of: loadedAudioAsset.tracks(withMediaType: .audio)[0],
+                                            at: kCMTimeZero)
+        } catch {
+            print("Failed to load Audio track")
         }
     }
     
-    //Ensure permission to access photo library
-    if PHPhotoLibrary.authorizationStatus() != .authorized {
-        PHPhotoLibrary.requestAuthorization { status in
-            if status == .authorized {
-                saveVideoToPhotos()
-            }
+    // 4 - Get Path
+    guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        else { return }
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .long
+    dateFormatter.timeStyle = .short
+    let date = dateFormatter.string(from: Date())
+    let url = documentDirectory.appendingPathComponent("mergeVideo-\(date).mov")
+    
+    // 5 - Create Exporter
+    guard let exporter = AVAssetExportSession(asset: mixComposition, presetName:
+        AVAssetExportPresetHighestQuality) else { return }
+    
+    exporter.outputURL = url
+    exporter.outputFileType = AVFileType.mov
+    exporter.shouldOptimizeForNetworkUse = true
+    
+    // 6 - Perform the Export
+    exporter.exportAsynchronously() {
+        DispatchQueue.main.async {
+            self.exportDidFinish(exporter)
         }
-    } else {
-        saveVideoToPhotos()
     }
+    
   }
-    
-  
-    
 }
 
 extension MergeVideoViewController: UIImagePickerControllerDelegate {
